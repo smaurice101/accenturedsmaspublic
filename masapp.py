2181f6f3435fddb42422243054d684e8bb80610e
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2018-06-17 14:02:37 -0400

    Accenture Multi-agent Accelerator for Data Science

---
+++ masapp.py
@@ -0,0 +1,5 @@
+import  accenture_ai_ds_mas
+
+
+
+    
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Multiagentaccelerator',
+    version='1.0',
+    description='Multi-agent accelerator for data science',
+    license='MIT',
+    packages=['C:\CORE_FILES\Accenture\VistraEnergy\python'],
+    author='Sebastian Maurice',
+    author_email='sebastian.maurice@gmail.com',
+    keywords=['multi-agent, data science, artificial intelligence'],
+)
+
